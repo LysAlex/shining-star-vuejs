d5d676f3fc1826c1faa56f0a060e16388a088e25
Alex LYS <alex@lys-phorimavong.com>
2025-09-08 15:08:37 +0200

    Events

--- app.js
+++ app.js
@@ -1,21 +1,17 @@
 const app = Vue.createApp({
   data() {
     return {
-      courseGoalA: 'Finish the course and learn Vue!',
-      courseGoalB: 'Master Vue and build amazing apps!',
-      vueLink: 'https://vuejs.org/'
+      counter: 0,
     };
   },
   methods: {
-    outputGoal: function() {
-      const randomNumber = Math.random();
-      if (randomNumber < 0.5) {
-        return this.courseGoalA;
-      } else {
-        return this.courseGoalB;
-      }
+    add(num) {
+      this.counter = this.counter + num;
+    },
+    reduce(num) {
+      this.counter = this.counter - num;
     }
-  } 
+  }
 });
 
-app.mount('#user-goal');
+app.mount('#events');
